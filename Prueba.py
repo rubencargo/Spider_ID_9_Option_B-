7551657116ee22300578c1e92c09ed7f0cb42464
rubencargo <rubencargo.93@gmail.com>
2020-12-29 17:21:05 +0100

    Actualizacion del programa con los ultimos datos corregidos de @mleonte. Falta corregir Salida

--- Prueba.py
+++ Prueba.py
@@ -19,50 +19,32 @@ url = requests.get(target_url).content
 soup = BeautifulSoup(url,'html.parser')
 # Mediante el for y el método de bs4 llamado find_all, recolectamos todas las etiquetas donde existe a href. #
 for line in soup.find_all('a'):
-	print(line)
 	new_line = line.get('href')
-	print(new_line)
-	try:
-		# Si existe en alguna línea del código fuente el http, lo almacenamos en nuestra lista llamada urls. #
-		if new_line[:4] == "http":
-			if target_url in new_line:
+	line_etiqueta = line.find(sys.argv[2])
+	if line_etiqueta != None: 
+		try:
+			# Si existe en alguna línea del código fuente el http, lo almacenamos en nuestra lista llamada urls. #
+			if new_line[:4] == "http":
 				urls.append(str(new_line))
-		# Si no existe, intentamos combinar nuestro argumento(url de la página) + lo que encontramos. #
-		elif new_line[:1] == "/":
-			try:
-				comb_line = target_url+new_line
-				urls.append(str(comb_line))
-			except:
-				pass
-		# Recorremos lo que hemos guardado anteriormente en nuestra lista(urls).
-		for get_this in urls:
-			# Como dentro de urls están todos los enlaces, realizamos una conexión a dicha url y leemos el código fuente. #
-			url = requests.get(get_this).content
-			# Usamos nuestra librería de bs4 para posteriormente recatar lo que deseamos. #
-			soup = BeautifulSoup(url,'html.parser')
-			for line in soup.find_all('a'):
-				new_line = line.get('href')
+			# Si no existe, intentamos combinar nuestro argumento(url de la página) + lo que encontramos. #
+			elif new_line[:1] == "/":
 				try:
-					if new_line[:4] == "http":
-						if target_url in new_line:
-							urls2.append(str(newline))
-					elif new_line[:1] == "/":
-						comb_line = target_url+new_line
-						urls2.append(str(comb_line))
+					comb_line = target_url+new_line
+					urls.append(str(comb_line))
 				except:
-					urls_3 = set(urls2)
-			# Recorremos nuestra lista llamada urls_3 e imprimimos todos los links de nuestro spider.
-			for value in urls_3:
-				print(value)
-	except:
+					pass
+			else:
+				#Enlaces fallidos como por ejemplo: javascript:void(0), lo añadimos para luego mostrar que en la ejecucion falla#
+				urls.append(str(new_line))
+		except:
+			pass
+	else:
 		pass
-
-
-#def main():
-#	buscador=input("¿Que estas buscando? :")
-#	localizacion=input("¿Donde?")
-#	input(buscador, localizacion)
-
-
-#main()
-
+#Accedemos a las paginas recogidas inicialmente y comprobamos si son accesibles o no#
+for get_this in urls:
+	if get_this[:4] == "http":
+		print('URL:',get_this)
+	elif get_this[:1] == "/":
+		print('URL:',get_this)
+	else:
+		print('No se puede acceder al enlace', get_this)
